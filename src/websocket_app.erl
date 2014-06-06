be09c60fe14e7007e6a8c00f67949060c5148759
wbykos <wbykos@gmail.com>
2014-06-06 14:39:54 +0400

    3

--- src/websocket_app.erl
+++ src/websocket_app.erl
@@ -12,24 +12,29 @@
 
 -define(ControlPort, 11111).
 -define(DstIp, "10.241.26.60").
--define(AvailablePorts, [11112,11113,11114,11115,11116]).
--define(PacketSize, 1408).
+-define(PacketSize, 1328).
 -define(ToDoFolder,"c:/f1").
 -define(CompletedFolder,"c:/f2").
--define(Pushups, 768).
--define(RestForPushups,500).
+%% -define(ChunkSize,40).
+-define(ChunkSize,16777216).
+-define(Threads,3).
+-define(Device,"\\Device\\NPF_{A79E0D8C-FD36-42F4-892D-B4214EB2B9F2}").
+-define(Head,[erlang:list_to_integer(K, 16) || K <- ["8c","89","a5","c7","f9","14","8c","89","a4","32","4d","d7","08","00","45","00","00","3c","0e","ec","00","00","80","11","e1","89","0a","f1","1a","1e","0a","f1","1a","3c","2b","68","2b","68","00","08","ba","bf"]]).
 
--export([start/2, stop/1, dir_loop/0, file_loop/0, zero_fill/2, file_prep/0, read_loop/0, overhead/2, move_and_clean/1, send_chunk/3]).
+%% K1 =[erlang:list_to_integer(K, 16) || K <- ["8c","89","a5","c7","f9","14","8c","89","a4","32","4d","d7","08","00","45","00","00","3c","0e","ec","00","00","80","11","e1","89","0a","f1","1a","1e","0a","f1","1a","3c","2b","68","2b","68","00","08","ba","bf"]],
+-export([start/2, stop/1, dir_loop/0, file_loop/0, zero_fill/2, read_loop/0, overhead/3, move_and_clean/1, send_chunk/6,send_control/1,get_free_thread/0,send_packet/4]).
 
 %% API.
 start(_Type, _Args) ->
-	ets:new(files, [public, named_table]),
-	ets:new(program, [public, named_table]),
+	ets:new(files, [public, named_table,bag]),
+	ets:new(program, [public, named_table, bag]),
 	register(dir_loop, spawn(fun() -> dir_loop() end)),
 	register(file_loop, spawn(fun() -> file_loop() end)),
 	case gen_udp:open(?ControlPort, [binary,{active, false}]) of
 		{ok, CtrlSocket} ->
 			ets:insert(program, {data,{coontrol_socket,CtrlSocket}}),
+			lists:takewhile(fun(X) -> ets:insert(program, {data,{thread,X,free}}) end, lists:seq(1,?Threads)),
+			register(send_control, spawn(?MODULE, send_control,[CtrlSocket])),
 			io:format("Success open control socket ~p~n",[CtrlSocket]);
 		{error, Reason} ->
 			io:format("Error open control socket ~p~n",[Reason]),
@@ -57,8 +62,6 @@ start(_Type, _Args) ->
 %% Point: Periodic scan files in ToDoFolder
 dir_loop() ->
 	receive
-		stop ->
-			void;
 		_ ->
 			dir_loop()
 		after 3000 ->
@@ -80,105 +83,25 @@ end.
 %% Point: Periodic scan files in file table
 file_loop() ->
 	receive
-		stop ->
-			void
 		after 1000 ->
 			case ets:match(files, {'$1',{status, none}},1) of
 				{[[File]],_} ->
-					io:format("Start preparation for file: ~p~n", [File]),
-					ets:insert(files, {File,{status, preparation}}),
-					Pid = spawn(fun() -> file_prep() end),
-					Pid ! {filename, File};
+					io:format("Start reading for file: ~p~n", [File]),
+					ets:delete_object(files,{File,{status,none}}),
+					ets:insert(files, {File,{status,reading}}),
+					Pid = spawn(fun() -> read_loop() end),
+					Pid ! {start, File};
 				_ ->
 					void
 			end,
-			%% io:format("File table content: ~p~n",[ets:match(files, '$1')]),
-			%%ets:insert(files, {Fi,ttt}),
 			file_loop()
 end.
 
-%% Point: Spawn read_loop fun, and before this, look over nonbusy ports
-%% Awarness: Posible delay queue on busy ports
-file_prep() ->
-	receive
-		stop ->
-			exit(omg);
-		{filename, File} ->
-			Pid = spawn(fun() -> read_loop() end),
-			lists:foreach(fun(E) -> E end, 
-			lists:takewhile(fun(E) -> case gen_udp:open(E, [binary,{active, false},{sndbuf, 16438000}]) of 
-											{ok, Socket} ->
-												io:format("Success test socket with port: ~p~n",[E]),
-												gen_udp:close(Socket),
-												ets:insert(files, {File,{status,reading},{pid,Pid},{port,E}}),
-												Pid ! {start,File},
-												false;
-											{error, Reason} ->
-												io:format("Could not open port: ~p, reason: ~p~n",[E,Reason]),
-												true;
-											_ ->
-												true
-										end
-				 						end, ?AvailablePorts));
-		Any ->
-			io:format("file_work error... ~p~n",[Any])
-	end.
+
 %% Point: Send chunk of file through udp and then close and others...
 %% Awarness: Selfexit from fun on error opening file, for delay them
 read_loop() ->
 	receive
-		{ok,Device,CRC,Socket,Port,Sequence,Ss} ->
-			case file:read(Device, ?PacketSize) of
-				{ok, Data} -> 
-					NewSequence = Sequence + 1,
-					case 0 =:= NewSequence rem ?Pushups of
-						true ->
-							timer:sleep(?RestForPushups);
-						_ ->
-							void
-					end,
-					NewCRC = erlang:crc32(CRC, Data),
-					{ok, OverHead} = overhead(NewSequence,NewCRC),
-					%% gproc:send({p,l, Port},{self(),Port, integer_to_list(?PacketSize)}),
-					%% gen_udp:send(Socket,?DstIp,Port,<<OverHead/binary,Data/binary>>),
-					K1 = [erlang:list_to_integer(K, 16) || K <- ["8c","89","a5","c7","f9","14","8c","89","a4","32","4d","d7","08","00","45","00","00","3c","0e","ec","00","00","80","11","e1","89","0a","f1","1a","1e","0a","f1","1a","3c","2b","68","2b","68","00","08","ba","bf"]],
-					ewpcap:write(Ss, [K1,<<OverHead/binary,Data/binary>>]),
-					self() ! {ok,Device,NewCRC,Socket,Port,NewSequence,Ss},
-					read_loop();
-				eof ->
-					case file:close(Device) of
-						ok ->
-							[{_,{_,CtrlSocket}}] = ets:lookup(program, data),
-							%% io:format("Close file.~n"),
-							case ets:match(files, {'$1',{status,reading},{pid,self()},{port,'_'}},1) of
-								{[[FileToClose]],_} ->
-									ok = move_and_clean(FileToClose),
-									io:format("Success close file, checksum is: ~p~n",[CRC]),
-									Info2 = erlang:iolist_to_binary([<<"0">>,zero_fill(erlang:size(unicode:characters_to_binary(FileToClose,unicode)),3),unicode:characters_to_binary(FileToClose,unicode),zero_fill(erlang:size(erlang:integer_to_binary(CRC)),12),erlang:integer_to_binary(CRC)]), 
-									gen_udp:send(CtrlSocket,?DstIp,?ControlPort,Info2),
-									io:format("Success close~p~n",[ewpcap:stats(Ss)]),
-									%% gproc:send({p,l, ws},{self(),ws,"checksum " ++ io_lib:format("~p",[CRC])}),
-									%% gproc:send({p,l, 11112},{self(),11112,"checksum " ++ io_lib:format("~p",[CRC])});
-									%% gproc:send({p,l,Port},{self(),Port, "close port " ++  io_lib:format("~p",[Port]) ++ " checksum " ++ io_lib:format("~p",[CRC])}),
-									gproc:send({p,l,ws},{self(),ws, "close port " ++  io_lib:format("~p",[Port]) ++ " checksum " ++ io_lib:format("~p",[CRC])});
-								['$end_of_table'] ->
-									io:format("Error. No file to close.~n")
-							end;
-						AnyFileErr ->
-							io:format("Error close file: ~p~n", [AnyFileErr]),
-							exit(error_eof)
-					end,
-					case gen_udp:close(Socket) of
-						ok ->
-							io:format("Success close data socket: ~p~n",[Socket]),
-							exit(all_good);
-						AnyUdpErr ->
-							io:format("Error close data socket: ~p~n",[AnyUdpErr])
-					end;
-				ReadError -> 
-					 io:format("Error reading: ~p~n", [ReadError]),
-					 exit(read_error)
-			end;
 		{start,File} ->
 			Device = case file:open(File, [read,raw,binary]) of
 						{ok, FileDevice} ->
@@ -197,77 +120,80 @@ read_loop() ->
 					ets:delete(files,File),
 					exit(file_read_info_error)
 			end,
-			[{_, {status,reading},{pid,_},{port,Port}}] = ets:lookup(files,File),
-			self ! {readchunk,Device,Port},
+			FileInfo = erlang:iolist_to_binary([<<"1">>,zero_fill(erlang:size(unicode:characters_to_binary(File,unicode)),3),unicode:characters_to_binary(File,unicode),zero_fill(erlang:size(erlang:integer_to_binary(Filesize)),12),erlang:integer_to_binary(Filesize)]), 
+			io:format("Started file ~p Size ~p on pid ~p~n",[File,Filesize,self()]),
+			send_control ! {send,FileInfo},
+			self() ! {readchunk,File,Device,1,0},
 			read_loop();
-		{readchunk,Device,Port} ->
-			case file:read(Device, 8192) of
+		{readchunk,File,Device,ChunkSeq,Offset} ->
+			{ok,_Pos} = file:position(Device,Offset),
+			%% io:format("Set to ~p~n",[Pos]),
+			case file:read(Device, ?ChunkSize) of
 				{ok, ChunkData} ->
-					[{_,{_,CtrlSocket}}] = ets:lookup(program, data),
-					%% {ok, Socket} = gen_udp:open(Port, [binary,{active, false},{sndbuf, 99438000}]),
-					Info = erlang:iolist_to_binary([<<"1">>,zero_fill(erlang:size(unicode:characters_to_binary(File,unicode)),3),unicode:characters_to_binary(File,unicode),zero_fill(erlang:size(erlang:integer_to_binary(Filesize)),12),erlang:integer_to_binary(Filesize),erlang:integer_to_binary(Port)]), 
-					gen_udp:send(CtrlSocket,?DstIp,?ControlPort,Info),
+					Thread = get_free_thread(),
 					timer:sleep(500),
-					io:format("Info ~p~n",[Info]),
-					gproc:send({p,l, ws},{self(),ws,"file " ++ io_lib:format("~p",[File])}),
-					gproc:send({p,l, ws},{self(),ws,"size " ++ io_lib:format("~p",[Filesize])}),
-					gproc:send({p,l, ws},{self(),ws,"port " ++ io_lib:format("~p",[Port])}),
-					spawn(?MODULE, send_chunk,[ChunkData,Port,self()]),
+					io:format("Chunk beg ~p Pid: ~p Thread: ~p Offset: ~p~n",[ChunkSeq,self(),Thread,Offset]),
+					%% gproc:send({p,l, ws},{self(),ws,"file " ++ io_lib:format("~p",[File])}),
+					%% gproc:send({p,l, ws},{self(),ws,"size " ++ io_lib:format("~p",[Filesize])}),
+					%% gproc:send({p,l, ws},{self(),ws,"port " ++ io_lib:format("~p",[Port])}),
+					ets:insert(files,{File,{chunk,ChunkSeq,packet,1}}),
+					spawn(?MODULE, send_chunk,[File,ChunkData,Thread,ChunkSeq,Offset,self()]),
+					self() ! {readchunk,File,Device,ChunkSeq+1,Offset+?ChunkSize},
 					read_loop();
 				eof ->
 					case file:close(Device) of
 						ok ->
-							gen_udp:send(CtrlSocket,?DstIp,?ControlPort,"empty"),
-							gproc:send({p,l, ws},{self(),ws,"Empty file"}),
-							case ets:match(files, {'$1',{status,reading},{pid,self()},{port,'_'}},1) of
-								{[[FileToClose]],_} ->
-									ok = move_and_clean(FileToClose),
-									io:format("Success close empty file.~n");
-								['$end_of_table'] ->
-									io:format("Error. No empty file to close.~n"),
-									exit(file_read_algoritm_error)
+							case ets:member(files, File) of
+								true ->	io:format("Success close file.~p~n",[File]),ets:insert(files,{File,{status,done}}), self() ! {done,File}, read_loop();
+								false -> io:format("Error. No file to close.~p~n",[File]),exit(file_read_algoritm_error)
 							end;
 						FileCloseError ->
-							io:format("Error. Can't close empty file ~p Error: ~p~n",[File, FileCloseError]),
+							io:format("Error. Can't close file ~p Error: ~p~n",[File, FileCloseError]),
 							ets:delete(files,File),
 							exit(file_close_error)
-					end,		
-					case gen_udp:close(Socket) of
-						ok ->
-							io:format("Success close empty socket: ~p~n",[Socket]);
-						UdpCloseError ->
-							io:format("Error close empty socket: ~p~n",[UdpCloseError])
 					end;
 				FileReadError ->
-					case ets:match(files, {'$1',{status,reading},{pid,self()},{port,'_'}},1) of
+					case ets:match(files, {'$1',{status,reading}},1) of
 						{[[FileToClose]],_} ->
 							ets:delete(files,FileToClose),
 							io:format("Read Error: ~p~n", [FileReadError]);
 						['$end_of_table'] ->
-							io:format("Error. No empty file to close.~n")
+							io:format("Error. No file to close.~n")
 					end,
 					exit(file_read_error)
 			end;
-		{done,Pid} ->
-			io:format("Done~p~n",[Pid]);		
+		{done,File} ->
+			Match1 = ets:match(files, {'$1',{status,done}}),
+			Match2 = ets:match(files, {File,{chunk,'$1',packets,'_'}}),
+			Match4 = ets:match(files, {File,{chunk,'$1',offset,'_'}}),
+			 %% io:format("E~p ttt ~p ttt ~p~n",[Match2,lists:sort(Match4),Match1]),
+				case [] =/= Match1 andalso lists:sort(Match2) == lists:sort(Match4) of
+					true ->
+								send_control ! {send,"File End"},
+								%% gproc:send({p,l, ws},{self(),ws,"file"}),
+								io:format("Table ~p~n",[ets:match(files, '$1')]),
+								ok = move_and_clean(File),
+								io:format("Done~p~n",[File]);
+
+					_Any ->
+						%% io:format("DDDD~p~n",[Any]),
+						read_loop()
+				end;	
 		UnknownData ->
-			io:format("Algorithm read_loop unknown data: ~p~n",[UnknownData]),
+			io:format("Read_loop unknown data: ~p~n",[UnknownData]),
 			%%exit(read_loop_error)
 			read_loop()
 	end.
 
-overhead(NewSequence,NewCRC) ->
+overhead(Data,File,ChunkSeq) ->
+	[[NewSequence]] = ets:match(files, {File,{chunk,ChunkSeq,packet,'$1'}}),
 	OverHeadSequence = zero_fill(NewSequence,8), 
-	OverHeadCRC = erlang:integer_to_binary(NewCRC),
-	OverHeadCRCsize = erlang:integer_to_binary(erlang:iolist_size(OverHeadCRC)),
-	case {erlang:iolist_size(OverHeadCRC) =< 9} of
-		{true} ->
-			OverHead = <<OverHeadSequence/binary,<<"0">>/binary, OverHeadCRCsize/binary, OverHeadCRC/binary>>;
-		{false} ->
-			OverHead = <<OverHeadSequence/binary,OverHeadCRCsize/binary, OverHeadCRC/binary>>
-	end,
-	%% io:format("Send data...~p~n",[OverHead]),
-	{ok,OverHead}.
+	OverHeadCRC = zero_fill(erlang:crc32(Data),12),
+	OverHead = <<OverHeadSequence/binary,OverHeadCRC/binary>>,
+	%% io:format("Send data...~p~n",[NewSequence]),
+	ets:delete_object(files,{File,{chunk,ChunkSeq,packet,NewSequence}}),
+	ets:insert(files,{File,{chunk,ChunkSeq,packet,NewSequence+1}}),
+	OverHead.
 
 zero_fill(Number, Size) ->
 	case type_of(Number) of
@@ -283,18 +209,68 @@ zero_fill(Number, Size) ->
 move_and_clean(FileToClose) ->
 	%% FileToMove = filename:join(lists:append([?CompletedFolder],lists:nthtail(erlang:length(filename:split(?ToDoFolder)),filename:split(FileToClose)))),
 	%% filelib:ensure_dir(FileToMove),
-	%% file:rename(FileToClose,FileToMove),
+	%% file:rename(FileToClose,FileToMove), 
 	ets:delete(files,FileToClose),
 	ok.
+%% 
+send_chunk(File,Data,Thread,ChunkSeq,Offset,Pid) ->
+		%% {ok, Socket} = ewpcap:open(?Device, [{filter, "icmp"},{buffer, 32438000}]),
+		io:format("Thread ~p~n",[Thread]),
+		{ok, Socket2} = gen_udp:open(22220+Thread,[binary,{active, false},{buffer, 16777216},{dontroute, true},{reuseaddr, true},{sndbuf, 16777216}]),
+		case erlang:size(Data) =< ?PacketSize of
+			true -> Len = erlang:size(Data);
+			false -> Len = ?PacketSize
+		end,
+		Len2 = Len+62,
+		ets:insert(files,{File,{chunk,ChunkSeq,offset,Offset}}),
+		CompiledData = erlang:list_to_binary([erlang:list_to_binary([?Head,erlang:list_to_binary([overhead(PieceOfSh,File,ChunkSeq),<<PieceOfSh/binary>>])]) || <<PieceOfSh:Len/binary>> <= Data]),
+		%% io:format("Data Len ~p Len2 ~p HERE ~p~n",[Len,Len2,CompiledData]),
+		%% [ewpcap:write(Socket,[<<PieceOfSh2/binary>>]) || <<PieceOfSh2:Len2/binary>> <= CompiledData],
+		[ send_packet(Socket2,"10.241.26.60",22220+Thread,PieceOfSh) || <<PieceOfSh:Len2/binary>> <= Data],
+		%% io:format("Chunk ~p end Pid: ~p Thread: ~p Stats: ~p~n",[ChunkSeq,self(),Thread,ewpcap:stats(Socket)]),
+		%% ok = ewpcap:close(Socket),
+		ok = gen_udp:close(Socket2),
+		[[SendedPackets]] = ets:match(files, {File,{chunk,ChunkSeq,packet,'$1'}}),
+		ets:delete_object(files,{File,{chunk,ChunkSeq,packet,SendedPackets}}),
+		ets:insert(files,{File,{chunk,ChunkSeq,packets,SendedPackets}}),
+		true = ets:delete_object(program, {data,{thread,Thread,active}}),
+		true = ets:insert(program, {data,{thread,Thread,free}}),
+		Pid ! {done, File},
+		timer:sleep(1000).
 
-send_chunk(Data,Port,Pid) ->
-		{ok, Socket} = ewpcap:open("\\Device\\NPF_{A79E0D8C-FD36-42F4-892D-B4214EB2B9F2}", [{filter, "icmp"},{buffer, 99438000}]),
-		K1 = [erlang:list_to_integer(K, 16) || K <- ["8c","89","a5","c7","f9","14","8c","89","a4","32","4d","d7","08","00","45","00","00","3c","0e","ec","00","00","80","11","e1","89","0a","f1","1a","1e","0a","f1","1a","3c","2b","68","2b","68","00","08","ba","bf"]],
-		CRC = erlang:crc32(Data),
-		{ok, OverHead} = overhead(1,CRC),
-		[ewpcap:write(Socket,[K1,<<OverHead/binary,PieceOfSh/binary>>]) || <<PieceOfSh:1400/binary>> <= Data],
-		io:format("EDone~p~n",[self()]),
-		Pid ! {done,self()}.
+send_packet(Socket,Ip,Port,Data) ->
+		gen_udp:send(Socket,Ip,Port,Data),
+		gproc:send({p,l, ws},{self(),ws,"1"}).
+
+get_free_thread() ->
+	Sorted = lists:sort(ets:match(program, {data,{thread,'$1',free}})),
+	case Sorted =/= [] of
+		true ->
+			[N] = lists:nth(1,Sorted),
+			case gen_udp:open(22220+N,[binary,{active, false},{dontroute, true},{reuseaddr, true}]) of
+				{ok,S} ->
+					gen_udp:close(S),
+					ets:delete_object(program, {data,{thread,N,free}}),
+					%% io:format("Sorted1 ~p~n",[ets:match(program, '$1')]),
+					ets:insert(program, {data,{thread,N,active}}),
+					%% io:format("Sorted2 ~p~n",[ets:match(program, '$1')]),
+					N;
+				{error,_} ->
+					timer:sleep(500),
+					get_free_thread()
+			end;
+		false ->
+			timer:sleep(500),
+			get_free_thread()
+	end.
+
+send_control(Socket) ->
+	receive
+		{send,Data} ->
+			gen_udp:send(Socket,?DstIp,?ControlPort,Data),
+			send_control(Socket)
+	end.
+
 
 
 type_of(X) when is_integer(X)   -> integer;
